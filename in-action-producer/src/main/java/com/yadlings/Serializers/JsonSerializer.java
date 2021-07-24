2813dcca30564a69acc927d1a449c4e2efd5e7c8
nindaba <arthurninda@gmail.com>
2021-07-24 19:43:47 +0200

    oduce working withall serializers

--- in-action-producer/src/main/java/com/yadlings/Serializers/JsonSerializer.java
+++ in-action-producer/src/main/java/com/yadlings/Serializers/JsonSerializer.java
@@ -1,16 +1,13 @@
 package com.yadlings.Serializers;
 
 import com.google.gson.Gson;
+import lombok.NoArgsConstructor;
 import org.apache.kafka.common.serialization.Serializer;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-
+@NoArgsConstructor
 public class JsonSerializer<T> implements Serializer<T> {
-    private Gson gSon;
-    JsonSerializer(){
-        this.gSon = new Gson();
-    }
     @Override
     public void configure(Map<String, ?> configs, boolean isKey) {
         Serializer.super.configure(configs, isKey);
@@ -18,7 +15,7 @@ public class JsonSerializer<T> implements Serializer<T> {
 
     @Override
     public byte[] serialize(String s, T t) {
-        return gSon.toJson(t).getBytes(StandardCharsets.UTF_8);
+        return new Gson().toJson(t).getBytes(StandardCharsets.UTF_8);
     }
     @Override
     public void close() {
--- in-action-producer/src/main/java/com/yadlings/inactionproducer/ProducerService.java
+++ in-action-producer/src/main/java/com/yadlings/inactionproducer/ProducerService.java
@@ -1,7 +1,6 @@
 package com.yadlings.inactionproducer;
 
 import com.yadlings.Domain.User;
-import com.yadlings.avro.StockTransaction;
 import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -25,28 +24,28 @@ import java.util.UUID;
 public class ProducerService {
 
     public void send1() {
-        Arrays.asList(
-                new StockTransaction("A","B",100,"1","8",40.0,new Date().getTime(),true),
-                new StockTransaction("B","A",50,"15","2",10.0,new Date().getTime(),true),
-                new StockTransaction("C","C",100,"41","1",45.0,new Date().getTime(),true),
-                new StockTransaction("D","C",70,"21","12",3.5,new Date().getTime(),true),
-                new StockTransaction("Z","A",4500,"11","B",25.21,new Date().getTime(),true),
-                new StockTransaction("Z","B",5300,"11","B",25.21,new Date().getTime(),true),
-                new StockTransaction("C","B",30,"1","2",0.9,new Date().getTime(),true),
-                new StockTransaction("A","B",100,"1","8",40.0,new Date().getTime(),true),
-                new StockTransaction("B","A",50,"15","2",10.0,new Date().getTime(),true),
-                new StockTransaction("C","C",100,"41","1",45.0,new Date().getTime(),true),
-                new StockTransaction("D","C",70,"21","12",3.5,new Date().getTime(),true),
-                new StockTransaction("Z","C",5200,"11","B",25.21,new Date().getTime(),true),
-                new StockTransaction("C","B",30,"1","2",0.9,new Date().getTime(),true),
-                new StockTransaction("C","B",100,"12","2",4.5,new Date().getTime(),true)
+//        Arrays.asList(
+//                new StockTransaction("A","B",100,"1","8",40.0,new Date().getTime(),true),
+//                new StockTransaction("B","A",50,"15","2",10.0,new Date().getTime(),true),
+//                new StockTransaction("C","C",100,"41","1",45.0,new Date().getTime(),true),
+//                new StockTransaction("D","C",70,"21","12",3.5,new Date().getTime(),true),
+//                new StockTransaction("Z","A",4500,"11","B",25.21,new Date().getTime(),true),
+//                new StockTransaction("Z","B",5300,"11","B",25.21,new Date().getTime(),true),
+//                new StockTransaction("C","B",30,"1","2",0.9,new Date().getTime(),true),
+//                new StockTransaction("A","B",100,"1","8",40.0,new Date().getTime(),true),
+//                new StockTransaction("B","A",50,"15","2",10.0,new Date().getTime(),true),
+//                new StockTransaction("C","C",100,"41","1",45.0,new Date().getTime(),true),
+//                new StockTransaction("D","C",70,"21","12",3.5,new Date().getTime(),true),
+//                new StockTransaction("Z","C",5200,"11","B",25.21,new Date().getTime(),true),
+//                new StockTransaction("C","B",30,"1","2",0.9,new Date().getTime(),true),
+//                new StockTransaction("C","B",100,"12","2",4.5,new Date().getTime(),true)
 //              new Purchase( new Long(1),new Long(1),"3467-4787-6534-8745",new Long(15),new Long(1),new Double(120), new Date().getTime())  ,
 //              new Purchase( new Long(2),new Long(1),"4765-7349-7345-2525",new Long(415),new Long(2),new Double(214),new Date().getTime())  ,
 //              new Purchase( new Long(3),new Long(2),"5245-8783-4245-5245",new Long(15),new Long(2),new Double(340),new Date().getTime())  ,
 //              new Purchase( new Long(4),new Long(1),"5425-4733-2654-4685",new Long(13),new Long(2),new Double(42),new Date().getTime())  ,
 //              new Purchase( new Long(4),new Long(3),"2767-6542-6722-7262",new Long(13),new Long(1),new Double(234),new Date().getTime())  ,
 //              new Purchase( new Long(5),new Long(3),"2542-8734-8463-0873",new Long(62),new Long(2),new Double(532),new Date().getTime())
-        ).forEach(this::send);
+//        ).forEach(this::send);
     }
     @Value("${topic.name}")
     private String topic;
@@ -79,20 +78,20 @@ public class ProducerService {
             }
         });
     }
-    private void send(StockTransaction purchase){
-        var record = new ProducerRecord<CharSequence,StockTransaction>(topic,purchase.getIndustry().toString(),purchase);
-        kafkaTemplate.send(record).addCallback(new ListenableFutureCallback() {
-            @Override
-            public void onFailure(Throwable throwable) {
-                log.error("The Purchase could not be written due {}",throwable);
-            }
-
-            @Override
-            public void onSuccess(Object o) {
-                log.info("Successfully written {}",o);
-            }
-        });
-    }
+//    private void send(StockTransaction purchase){
+//        var record = new ProducerRecord<CharSequence,StockTransaction>(topic,purchase.getIndustry().toString(),purchase);
+//        kafkaTemplate.send(record).addCallback(new ListenableFutureCallback() {
+//            @Override
+//            public void onFailure(Throwable throwable) {
+//                log.error("The Purchase could not be written due {}",throwable);
+//            }
+//
+//            @Override
+//            public void onSuccess(Object o) {
+//                log.info("Successfully written {}",o);
+//            }
+//        });
+//    }
     @Bean
     public NewTopic newTopic(){
         return new NewTopic(topic,partitions,replicas);
