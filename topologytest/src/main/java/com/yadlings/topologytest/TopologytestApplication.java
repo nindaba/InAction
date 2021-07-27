9bece52d935d3928cf956dbe0e695541e7619814
shohruzjon-tech <abdivakilov1981@gmail.com>
2021-07-27 18:57:22 +0200

    Topology Test final commit

---
+++ topologytest/src/main/java/com/yadlings/topologytest/TopologytestApplication.java
@@ -0,0 +1,13 @@
+package com.yadlings.topologytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopologytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopologytestApplication.class, args);
+	}
+
+}
